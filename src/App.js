e99668a3b4498ae36b6659917152b3d329a15650
tworrell <tara.worrell@gmail.com>
2020-02-25 20:38:05 -0500

    Added logic for game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PlayerCard from './PlayerCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Game extends Component {
+  constructor() {
+    super();
+    this.signs = ["rock", "scissors", "paper"]
+    this.state ={
+      playerOne: "rock",
+      playerTwo: "scissors",
+    }
+  }
+
+  //this is where my methods will go :)
+  playGame = () => {
+    this.setState({
+      playerOne: this.signs[Math.floor(Math.random()*3)],
+      playerTwo: this.signs[Math.floor(Math.random()*3)],
+    })
+  }
+
+  decideWinner = () => {
+    const playerOne = this.state.playerOne;
+    const playerTwo = this.state.playerTwo;
+
+    if(playerOne === playerTwo) {
+      return "It's a Tie!"
+    } else if ((playerOne === 'rock' && playerTwo === 'scissors') ||
+    (playerOne === 'scissors' && playerTwo === 'paper') ||
+    (playerOne === 'paper' && playerTwo === 'rock')) {
+      return "Player One Wins!!";
+    }else {
+      return "Player Two Wins!!";
+    }
+
+  }
+
+
+  render(){
+    return(
+      <div className="style">
+        <div>
+        <PlayerCard sign ={this.state.playerOne}/>
+        <PlayerCard sign ={this.state.playerTwo}/>
+        </div>
+        <div className= "winner">Place the winner here</div>
+        <button type="button" onClick={this.playGame}>Play the Game</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Game;
---
+++ src/PlayerCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlayerCard = (props) => {
+  return (
+    <div className="player-card">{props.sign}</div>
+  )
+}
+
+export default PlayerCard;
